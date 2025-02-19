1f6d3fb5a9ecab21c9af4f7da19cd042921d34e0
Kauê de Souza Silva <145780136+ConfuseKarma@users.noreply.github.com>
2025-02-18 21:29:34 -0300

    Update ValidaRG.java

--- src/main/java/com/automato_rg_cpf/ValidaRG.java
+++ src/main/java/com/automato_rg_cpf/ValidaRG.java
@@ -2,31 +2,30 @@ package com.automato_rg_cpf;
 
 public class ValidaRG {
 
-     public static boolean validarRG(String rg) {
+    public static boolean validarRG(String rg) {
         rg = rg.replaceAll("[^0-9X]", "");
         if (rg.length() < 7 || rg.length() > 9) {
             return false;
         }
-    
+
         // Verifica se 'X' aparece em qualquer posição que não seja a última
         for (int i = 0; i < rg.length() - 1; i++) {
             if (rg.charAt(i) == 'X') {
                 return false; // 'X' só pode estar no último dígito
             }
         }
-    
+
         int soma = 0, peso = rg.length();
         for (int i = 0; i < rg.length() - 1; i++) {
             soma += Character.getNumericValue(rg.charAt(i)) * peso--;
         }
-    
+
         int resto = soma % 11;
         int digitoEsperado = (resto == 10) ? 0 : resto;
-    
+
         char ultimoChar = rg.charAt(rg.length() - 1);
         int ultimoNumero = (ultimoChar == 'X') ? 10 : Character.getNumericValue(ultimoChar);
-    
+
         return digitoEsperado == ultimoNumero;
     }
-
 }
